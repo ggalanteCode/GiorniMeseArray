b555510c78e5c397859c20d25edbcbc8f5470a09
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-06 10:45:04 +0200

    inserisco il numero corrispondente al mese e visualizzo il quanti giorni ha quel mese

--- src/com/soprasteria/giornimesearray/Main.java
+++ src/com/soprasteria/giornimesearray/Main.java
@@ -8,9 +8,17 @@ public class Main {
 		// TODO Auto-generated method stub
 		String[] mesiAnno = {"gennaio","febbraio", "marzo", "aprile", "maggio", "giugno",
 				"luglio","agosto", "settembre", "ottobre", "novembre", "dicembre"};
+		int[] giorniDelMese = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int numeroMese;
 		
 		Scanner s = new Scanner(System.in);
 		
+		System.out.println("Che giorno del mese vuoi?: ");
+		numeroMese = s.nextInt();
+		s.nextLine();
+		
+		System.out.println("Il numero di giorni di " + mesiAnno[numeroMese] + " è " 
+							+ giorniDelMese[numeroMese]);
 		
 	}
 
