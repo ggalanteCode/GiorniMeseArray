dec450a524baa92b726988cb9fa0fbeb7066151e
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-06 10:36:21 +0200

    inizializzato l'array dei giorni del mese

--- src/com/soprasteria/giornimesearray/Main.java
+++ src/com/soprasteria/giornimesearray/Main.java
@@ -1,9 +1,16 @@
 package com.soprasteria.giornimesearray;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] mesiAnno = {"gennaio","febbraio", "marzo", "aprile", "maggio", "giugno",
+				"luglio","agosto", "settembre", "ottobre", "novembre", "dicembre"};
+		
+		Scanner s = new Scanner(System.in);
+		
 		
 	}
 
